7fd37e899610fa4c578a70ccbd4163d3c9a9f949
Gregory <ggetchell@gmail.com>
2021-02-09 13:47:36 -0500

    Completed First Lab work

--- 01_python_fundamentals/01_07_area_perimeter.py
+++ 01_python_fundamentals/01_07_area_perimeter.py
@@ -3,3 +3,11 @@
 Write the necessary code to display the area and perimeter of a rectangle that has a width of 2.4 and a height of 6.4.
 
 '''
+
+width = float(input("Enter the width: "))
+height = float(input("Enter the height: "))
+area = width * height
+perimeter = 2 * (width + height)
+print("Area : {0:.2f}".format(area))
+print("Perimeter : {:.2f}".format (perimeter))
+
